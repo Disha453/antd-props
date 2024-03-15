49009b6edb117d112c6cf020410ae39804f44ef7
Disha453 <dishasanghani.dreamspot@gmail.com>
2024-03-15 14:54:46 -0700

    antd-props

---
+++ src/table.js
@@ -0,0 +1,25 @@
+export function TableData({ recordsData, deleteRecord }) {
+
+
+    return (
+        <table>
+            <thead>
+                <th>Fname</th>
+                <th>Age</th>
+                <th>Password</th>
+            </thead>
+            <tbody>
+                {recordsData?.map((item, index) => {
+                    return (
+                        <tr>
+                            <td>{item.fname}</td>
+                            <td>{item.age}</td>
+                            <td>{item.password}</td>
+                            <td><button type="delet" onClick={() => deleteRecord(index)} className="bg-orange-800 h-12 w-24 rounded-xl  border-yellow-300  text-white">Delet</button></td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
